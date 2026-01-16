d21b216ea7f895c377a18c58bec8dbbabc6237d1
yalmar-guette <yalmarguette@outlook.com>
2026-01-16 09:31:45 -0400

    Fix main.jsx import and deploy crash handler

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ToastProvider } from './contexts/ToastContext'
 import GlobalErrorBoundary from './components/GlobalErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
